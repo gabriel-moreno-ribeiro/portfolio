7c3989db72bebeab63b96538949460126836fe21
AviVashishta29 <avivashishta@gmail.com>
2026-02-24 22:43:27 +0530

    adding posthog

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+import posthog from "posthog-js";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
+
+posthog.init("phc_cgNNpL9lqLK50jeJICAV6xcGZDmuDnuPVxPeG8Ieg6m", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+  capture_pageview: true,
+  capture_pageleave: true,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
